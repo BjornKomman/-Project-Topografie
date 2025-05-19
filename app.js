12e8a48a72ddeee2c2b7dc9dda931da91c82572c
Bjorn Komman <bjorn.komman@student.graafschapcollege.nl>
2025-05-19 14:05:59 +0200

    boundries?

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
-const map = L.map('map').setView([20, 0], 2);
+const map = L.map('map', {
+  zoomControl: false,
+  scrollWheelZoom: false,
+  doubleClickZoom: false,
+  boxZoom: false,   
+  touchZoom: false,
+  dragging: false
+});
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
+const europeBounds = [
+  [34.5, -25.0],
+  [72.0, 45.0]
+];
+
+map.fitBounds(europeBounds);
+
+let countriesLayer;
+
+fetch('https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson')
+  .then(response => response.json())
+  .then(data => {
+    countriesLayer = L.geoJSON(data, {
+      style: {
+        color: "#3388ff",
+        weight: 1,
+        fillOpacity: 0.2
+      },
+      onEachFeature: function (feature, layer) {
+        layer.on('click', function () {
+          console.log("Clicked feature properties:", feature.properties);
+
+          const name =
+            feature.properties.ADMIN ||
+            feature.properties.NAME ||
+            feature.properties.name ||
+            "Unknown Country";
+
+          alert("You clicked on: " + name);
+        });
+      }
     }).addTo(map);
-
-    let countriesLayer;
-
-    fetch('https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson')
-      .then(response => response.json())
-      .then(data => {
-        countriesLayer = L.geoJSON(data, {
-          style: {
-            color: "#3388ff",
-            weight: 1,
-            fillOpacity: 0.2
-          },
-          onEachFeature: function (feature, layer) {
-            layer.on('click', function () {
-              console.log("Clicked feature properties:", feature.properties); // Debug
-
-              // Try multiple possible fields
-              const name =
-                feature.properties.ADMIN ||
-                feature.properties.NAME ||
-                feature.properties.name ||
-                "Unknown Country";
-
-              alert("You clicked on: " + name);
-            });
-          }
-        }).addTo(map);
-      });
+  });
