bcc1266a44f1cfe4ba366fad23523d03d9d90eef
Nick <nick.sutterland@student.graafschapcollege.nl>
2025-05-26 14:31:11 +0200

    quiz element toegevoegd,antwoord is goed of fout

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const map = L.map('map', {
   zoomControl: false,
   scrollWheelZoom: false,
   doubleClickZoom: false,
-  boxZoom: false,   
+  boxZoom: false,
   touchZoom: false,
   dragging: false
 });
@@ -19,6 +19,26 @@ let countriesLayer;
 fetch('https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson')
   .then(response => response.json())
   .then(data => {
+
+    console.log(data.features);
+
+    const allCountries = data.features.length;
+
+    const randNum = Math.floor(Math.random() * allCountries);
+
+    const randCountry = data.features[randNum].properties.name;
+
+    const infoBox = document.querySelector(".question");
+    infoBox.textContent = "Click on: " + randCountry;
+
+    fetch("european_countries.json")
+      .then((response) => response.json())
+      .then(json => {
+        let countries = json
+        console.log(countries);
+
+      })
+
     countriesLayer = L.geoJSON(data, {
       style: {
         color: "#3388ff",
@@ -26,16 +46,19 @@ fetch('https://raw.githubusercontent.com/datasets/geo-countries/master/data/coun
         fillOpacity: 0.2
       },
       onEachFeature: function (feature, layer) {
-        layer.on('click', function () {
-          const name =
-            feature.properties.name ||
-            "Unknown Country";
+        layer.on('click', function (item) {
 
-          const infoBox = document.getElementsByClassName("yes")[0];
-          if (infoBox) {
-            infoBox.innerHTML = "Country you clicked: " + name;
-          } else {
-            console.warn('Element with class "yes" not found.');
+          const name = feature.properties.name || "Unknown Country";
+
+          const answerBox = document.querySelector(".answer");
+
+          console.log(item);
+
+          if (name == randCountry) {
+            answerBox.textContent = "Correct!";
+          }
+          else {
+            answerBox.textContent = "Wrong!"
           }
         });
       }
