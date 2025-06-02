710cb547f7733e07fe2a11879fb4d0122aaca1ba
Nick <nick.sutterland@student.graafschapcollege.nl>
2025-06-02 16:43:41 +0200

    unieke europese landen

--- app.js
+++ app.js
@@ -24,47 +24,54 @@ fetch('https://raw.githubusercontent.com/datasets/geo-countries/master/data/coun
     let currentQuestion = 0;
     let score = 0;
 
-    for (let i = 0; i < 10; i++) {
-      const randNum = Math.floor(Math.random() * allCountries);
-      const randCountry = data.features[randNum].properties.name;
-      selectedCountries.push(randCountry);
-    }
+    fetch('european_countries.json')
+      .then(response => response.json())
+      .then(europeanData => {
+        const countriesPool = [...europeanData];
+        for (let i = 0; i < 10 && countriesPool.length > 0; i++) {
+          const randNum = Math.floor(Math.random() * countriesPool.length);
+          const randCountry = countriesPool.splice(randNum, 1)[0];
+          selectedCountries.push(randCountry);
+        }
 
-    const infoBox = document.querySelector(".question");
-    infoBox.textContent = "Click on: " + selectedCountries[currentQuestion];
+        console.log(selectedCountries);
 
-    countriesLayer = L.geoJSON(data, {
-      style: {
-        color: "#3388ff",
-        weight: 1,
-        fillOpacity: 0.2
-      },
-      onEachFeature: function (feature, layer) {
-        layer.on('click', function (item) {
-          const name = feature.properties.name || "Unknown Country";
-          const answerBox = document.querySelector(".answer");
-          const scoreBox = document.querySelector(".score");
+        const infoBox = document.querySelector(".question");
+        infoBox.textContent = "Click on: " + selectedCountries[currentQuestion];
 
-          if (currentQuestion < selectedCountries.length) {
-            if (name == selectedCountries[currentQuestion]) {
-              answerBox.style.color = 'green';
-              answerBox.textContent = "Correct!";
-              score++;
-            } else {
-              answerBox.style.color = 'red';
-              answerBox.textContent = "Wrong!";
-            }
-            currentQuestion++;
-            if (currentQuestion < selectedCountries.length) {
-              infoBox.textContent = "Click on: " + selectedCountries[currentQuestion];
-            } else {
-              infoBox.textContent = "Quiz finished!";
-              answerBox.style.color = 'black';
-              answerBox.textContent = 'Uw score van 0-100:';
-              scoreBox.textContent = score * 10;
-            }
+        countriesLayer = L.geoJSON(data, {
+          style: {
+            color: "#3388ff",
+            weight: 1,
+            fillOpacity: 0.2
+          },
+          onEachFeature: function (feature, layer) {
+            layer.on('click', function (item) {
+              const name = feature.properties.name || "Unknown Country";
+              const answerBox = document.querySelector(".answer");
+              const scoreBox = document.querySelector(".score");
+
+              if (currentQuestion < selectedCountries.length) {
+                if (name == selectedCountries[currentQuestion]) {
+                  answerBox.style.color = 'green';
+                  answerBox.textContent = "Correct!";
+                  score++;
+                } else {
+                  answerBox.style.color = 'red';
+                  answerBox.textContent = "Wrong!";
+                }
+                currentQuestion++;
+                if (currentQuestion < selectedCountries.length) {
+                  infoBox.textContent = "Click on: " + selectedCountries[currentQuestion];
+                } else {
+                  infoBox.textContent = "Quiz finished!";
+                  answerBox.style.color = 'black';
+                  answerBox.textContent = 'Uw score van 0-100:';
+                  scoreBox.textContent = score * 10;
+                }
+              }
+            });
           }
-        });
-      }
-    }).addTo(map);
+        }).addTo(map);
+      });
   });
